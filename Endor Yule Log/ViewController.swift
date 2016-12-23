ed6ccf1aeb3ea8df6b2ed8dcbc31858811485c7a
Mike Drum <mdrum@resultsdirect.com>
2016-12-23 16:06:04 -0700

    Initial Commit

---
+++ Endor Yule Log/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Endor Yule Log
+//
+//  Created by Mike Drum on 12/23/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
