cdc95c7eee3b42f29876ddd8f2ae998276241b85
Mike Drum <mdrum@resultsdirect.com>
2016-12-23 23:11:52 -0700

    initial concept: launches to looping vader/crackling

--- Endor Yule Log/ViewController.swift
+++ Endor Yule Log/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+  
+  private var playerController: AVPlayerViewController!
+  private var player: AVPlayer!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // create player
+    let playerItem = AVPlayerItem(asset: self.createComposition())
+    let player = AVPlayer(playerItem: playerItem)
+    
+    // make player loop the last minute
+    let time = NSValue(time: CMTime(seconds: 120, preferredTimescale: 1))
+    player.addBoundaryTimeObserver(forTimes: [time], queue: nil) {
+      player.seek(to: CMTime(seconds: 60, preferredTimescale: 1))
+    }
+    
+    // create controller
+    let controller = AVPlayerViewController()
+    controller.player = player
+    
+    self.playerController = controller
+    self.player = player
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    // present the controller
+    present(self.playerController, animated: true) {
+      self.player.play()
+    }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+  private func createComposition() -> AVComposition {
+    let composition = AVMutableComposition()
+    
+    var url = Bundle.main.url(forResource: "crackling", withExtension: "mp3")!
+    var asset = AVURLAsset(url: url)
+    var track = composition.addMutableTrack(withMediaType: AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID(exactly: 0)!)
+    do {
+      try track.insertTimeRange(CMTimeRange(start: kCMTimeZero, duration: asset.duration), of: asset.tracks(withMediaType: AVMediaTypeAudio).first!, at: kCMTimeZero)
+    } catch {
+      print("damn")
+    }
+    
+    url = Bundle.main.url(forResource: "Shortened-vader", withExtension: "mp4")!
+    asset = AVURLAsset(url: url)
+    track = composition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID(exactly: 1)!)
 
+    do {
+      try track.insertTimeRange(CMTimeRange(start: kCMTimeZero, duration: asset.duration), of: asset.tracks(withMediaType: AVMediaTypeVideo).first!, at: kCMTimeZero)
+    } catch {
+      print("damn")
+    }
+    
+    return composition
+  }
 
 }
 
